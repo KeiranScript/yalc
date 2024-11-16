1177e7da78113992dcb40540193011b30e1039ed
Keiran <keiran0@proton.me>
2024-11-16 13:48:17 +0000

    djsaidfjsafjiajfsoas

--- src/macros.rs
+++ src/macros.rs
@@ -1,39 +1,63 @@
 #[macro_export]
 macro_rules! info {
-    ($msg:expr $(, $disp_val:expr)?) => {{
+    ($msg:expr) => {{
         let logger = $crate::logger::get_logger();
         let logger = logger.lock().unwrap();
-        let display = $crate::get_display(logger.is_debug(), Some($disp_val.unwrap_or(true)));
+        let display = $crate::get_display(logger.is_debug(), None);
+        logger.log($crate::logger::LogLevel::Info, &format!($msg), display);
+    }};
+    ($msg:expr, $disp_val:expr) => {{
+        let logger = $crate::logger::get_logger();
+        let logger = logger.lock().unwrap();
+        let display = $crate::get_display(logger.is_debug(), Some($disp_val));
         logger.log($crate::logger::LogLevel::Info, &format!($msg), display);
     }};
 }
 
 #[macro_export]
 macro_rules! warn {
-    ($msg:expr $(, $disp_val:expr)?) => {{
+    ($msg:expr) => {{
         let logger = $crate::logger::get_logger();
         let logger = logger.lock().unwrap();
-        let display = $crate::get_display(logger.is_debug(), Some($disp_val.unwrap_or(true)));
+        let display = $crate::get_display(logger.is_debug(), None);
+        logger.log($crate::logger::LogLevel::Warn, &format!($msg), display);
+    }};
+    ($msg:expr, $disp_val:expr) => {{
+        let logger = $crate::logger::get_logger();
+        let logger = logger.lock().unwrap();
+        let display = $crate::get_display(logger.is_debug(), Some($disp_val));
         logger.log($crate::logger::LogLevel::Warn, &format!($msg), display);
     }};
 }
 
 #[macro_export]
 macro_rules! error {
-    ($msg:expr $(, $disp_val:expr)?) => {{
+    ($msg:expr) => {{
         let logger = $crate::logger::get_logger();
         let logger = logger.lock().unwrap();
-        let display = $crate::get_display(logger.is_debug(), Some($disp_val.unwrap_or(true)));
+        let display = $crate::get_display(logger.is_debug(), None);
+        logger.log($crate::logger::LogLevel::Error, &format!($msg), display);
+    }};
+    ($msg:expr, $disp_val:expr) => {{
+        let logger = $crate::logger::get_logger();
+        let logger = logger.lock().unwrap();
+        let display = $crate::get_display(logger.is_debug(), Some($disp_val));
         logger.log($crate::logger::LogLevel::Error, &format!($msg), display);
     }};
 }
 
 #[macro_export]
 macro_rules! success {
-    ($msg:expr $(, $disp_val:expr)?) => {{
+    ($msg:expr) => {{
         let logger = $crate::logger::get_logger();
         let logger = logger.lock().unwrap();
-        let display = $crate::get_display(logger.is_debug(), Some($disp_val.unwrap_or(true)));
+        let display = $crate::get_display(logger.is_debug(), None);
+        logger.log($crate::logger::LogLevel::Success, &format!($msg), display);
+    }};
+    ($msg:expr, $disp_val:expr) => {{
+        let logger = $crate::logger::get_logger();
+        let logger = logger.lock().unwrap();
+        let display = $crate::get_display(logger.is_debug(), Some($disp_val));
         logger.log($crate::logger::LogLevel::Success, &format!($msg), display);
     }};
 }
